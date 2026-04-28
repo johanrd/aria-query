73a75d1fc6311e1465ca1584f5c776882f0ea35d
Johan Røed <johanroed@gmail.com>
2026-04-28 11:07:04 +0200

    adress review: dont use Set

--- src/isInteractiveRole.js
+++ src/isInteractiveRole.js
@@ -5,21 +5,23 @@
 import rolesMap from './rolesMap';
 
 // Roles that are widget descendants but do not accept user input in practice.
-const OVERRIDE_NON_INTERACTIVE: Set<string> = new Set(['progressbar']);
+const OVERRIDE_NON_INTERACTIVE: Array<string> = ['progressbar'];
 
 // Roles that are not widget descendants but do accept user input in practice.
-const interactiveRoles: Set<string> = new Set(['toolbar']);
+const OVERRIDE_INTERACTIVE: Array<string> = ['toolbar'];
+
+const interactiveRoles: Array<string> = OVERRIDE_INTERACTIVE.slice();
 
 for (const [name, def] of rolesMap.entries()) {
   if (
     !def.abstract &&
-    !OVERRIDE_NON_INTERACTIVE.has(name) &&
+    OVERRIDE_NON_INTERACTIVE.indexOf(name) === -1 &&
     def.superClass.some((chain) => chain.indexOf('widget') !== -1)
   ) {
-    interactiveRoles.add(name);
+    interactiveRoles.push(name);
   }
 }
 
 export default function isInteractiveRole(role: string): boolean {
-  return interactiveRoles.has(role);
+  return interactiveRoles.indexOf(role) !== -1;
 }
